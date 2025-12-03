4e2d55b844b3d0e3692b7622d12cbae8b39ce00c
parksubeom <sooknise@naver.com>
2025-12-03 11:24:08 +0900

    build: github pages 배포 설정

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,12 @@ import react from '@vitejs/plugin-react-swc';
 export default mergeConfig(
   defineConfig({
     plugins: [react()],
+      build: {
+      rollupOptions: {
+        input: "./index.basic.html",
+      },
+    },
+    base: process.env.VITE_BASE_PATH || "/",
   }),
   defineTestConfig({
     test: {
